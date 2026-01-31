c386dcf1f3c5799bd61244b194dd0ae01615afb6
Treikeh <73736974+Treikeh@users.noreply.github.com>
2026-01-31 11:19:04 +0000

    Gave the ball and the trigger new colors

--- ball.lua
+++ ball.lua
@@ -2,28 +2,30 @@ local ball = {
     x = 100,
     y = 100,
     r = 10,
+    body,
+    shape,
+    fixture,
 }
 
 
-function spawnBall(world)
+function spawnBall()
     ball.body = love.physics.newBody(world, ball.x, ball.y, "dynamic")
     ball.shape = love.physics.newCircleShape(ball.r)
     ball.fixture = love.physics.newFixture(ball.body, ball.shape)
+    ball.fixture:setUserData("Ball")
 end
 
 
 function drawBall()
     love.graphics.print(ball.y, 40, 40)
-    love.graphics.circle("fill", ball.x, ball.y, ball.r)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.circle("fill", ball.body:getX(), ball.body:getY(), ball.shape:getRadius())
 end
 
 
 function updateBall()
-    ball.x, ball.y = ball.body:getPosition()
-end
-
-
-function collisionOnEnter(fixture_a, fixture_b, contact)
-    local dx, dy = contact:getNormal()
-    local point = {contact:getPositions()}
+    if ball then
+        ball.x, ball.y = ball.body:getPosition()
+    end
 end
---
+++ endTrigger.lua
@@ -0,0 +1,51 @@
+local trigger = {
+    x = 100,
+    y = 300,
+    w = 80,
+    h = 40,
+    body,
+    shape,
+    fixture,
+}
+
+local message = ""
+
+
+function spawnTrigger()
+    trigger.body = love.physics.newBody(world, trigger.x, trigger.y, "static")
+    trigger.shape = love.physics.newRectangleShape(trigger.w, trigger.h)
+    trigger.fixture = love.physics.newFixture(trigger.body, trigger.shape)
+    trigger.fixture:setUserData("Trigger")
+    trigger.fixture:setSensor(true)
+
+    world:setCallbacks(beginContact, endContact)
+end
+
+
+function drawTrigger()
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.polygon("fill", trigger.body:getWorldPoints(trigger.shape:getPoints()))
+    
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print(message, 40, 100)
+end
+
+
+
+function beginContact(fixtureA, fixtureB, contact)
+    local tag1, tag2 = fixtureA:getUserData(), fixtureB:getUserData()
+
+    if tag1 == "Trigger" and tag2 == "Ball" then
+        message = tag1 .. " collided with " .. tag2
+        changeGameState("reset")
+    end
+end
+
+
+function endContact(fixtureA, fixtureB, contact)
+    local tag1, tag2 = fixtureA:getUserData(), fixtureB:getUserData()
+
+    if tag1 == "Trigger" and tag2 == "Ball" then
+        message = ""
+    end
+end
